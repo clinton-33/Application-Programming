e478f336c7700458c18c106f965df6c0d308e29b
clinton-33 <147735133+clinton-33@users.noreply.github.com>
2024-12-02 17:30:18 +0300

    Revert "Error correction (#5)"
    This reverts commit 63defbe11fa25af919d9665cff9d6b4386e0352e.

--- TransactionInterface.java
+++
@@ -1,9 +0,0 @@
-package transactionsystem;
-import java.util.Calendar;
-public interface TransactionInterface {
-    double getAmount();
-    Calendar getDate();
-    String getTransactionID();
-    void apply(BankAccount ba);
-    void printTransactionDetails();
-}
