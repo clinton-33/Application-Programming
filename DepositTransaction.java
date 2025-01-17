4097a7d485094d34bc726347ab0768ddabea5077
clinton-33 <147735133+clinton-33@users.noreply.github.com>
2025-01-17 05:09:00 +0300

    Update DepositTransaction.java

--- DepositTransaction.java
+++ DepositTransaction.java
@@ -1,18 +1,32 @@
-/**
- * Represents a deposit transaction.
- */
 package transactionsystem;
 
 import java.util.Calendar;
 
+/**
+ * Represents a deposit transaction.
+ */
 public class DepositTransaction extends BaseTransaction {
-    public DepositTransaction(double amount, Calendar date, String transactionID) {
-        super(amount, date, transactionID);
+
+    /**
+     * Constructs a DepositTransaction with the specified amount and date.
+     * 
+     * @param amount the amount to deposit.
+     * @param date   the date of the transaction.
+     */
+    public DepositTransaction(double amount, Calendar date) {
+        super(amount, date);
     }
 
+    /**
+     * Applies the deposit transaction to the specified bank account.
+     * 
+     * @param bankAccount the bank account to which the deposit will be applied.
+     */
     @Override
-    public void apply(BankAccount ba) {
-        ba.deposit(amount);
+    public void apply(BankAccount bankAccount) {
+        bankAccount.deposit(amount);
+        System.out.println("Deposit applied.");
     }
 }
+
 
