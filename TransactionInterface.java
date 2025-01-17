047ef98011f61f7383b8db93f1e2f00751f66d5b
clinton-33 <147735133+clinton-33@users.noreply.github.com>
2025-01-17 04:59:46 +0300

    Update TransactionInterface.java

--- TransactionInterface.java
+++ TransactionInterface.java
@@ -1,18 +1,39 @@
-package transactionsystem;
-/*
+import java.util.Calendar;
+
+/**
  * Interface defining the blueprint for financial transactions.
  */
-import java.util.Calendar;
 public interface TransactionInterface {
+    /**
+     * Gets the amount of the transaction.
+     * 
+     * @return the transaction amount as a double.
+     */
     double getAmount();
+
+    /**
+     * Gets the date of the transaction.
+     * 
+     * @return the transaction date as a Calendar object.
+     */
     Calendar getDate();
+
+    /**
+     * Gets the unique identifier of the transaction.
+     * 
+     * @return the transaction ID as a String.
+     */
     String getTransactionID();
+
+    /**
+     * Prints the transaction details.
+     */
+    void printTransactionDetails();
+
     /**
      * Applies the transaction to the given bank account.
-     *
-     * @param BankAccount the bank account to which the transaction will be applied.
+     * 
+     * @param bankAccount the bank account to which the transaction will be applied.
      */
-    void apply(BankAccount ba);
-
-    void printTransactionDetails();
+    void apply(BankAccount bankAccount);
 }
